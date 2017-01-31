9db2688dd4c004ec42bd18cb7b9e421347ac5312
Diego <dm1681@uw.edu>
2017-01-31 15:41:14 -0800

    cleanup

--- Research/Merging_Perts/HistEcc.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-data = np.random.randint(1000) #replace this with the ecc data
-
-np.histogram(data,range=(0,0.05)) # histogram w/ bin width 0.05
